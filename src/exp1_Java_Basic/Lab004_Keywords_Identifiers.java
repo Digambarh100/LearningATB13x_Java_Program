7f8cb8f51ca66c726db3f42f401f3fd7a20660ea
Digambarh100 <digambarh100@gmail.com>
2025-06-09 22:17:55 +0530

    Test2

--- src/exp1_Java_Basic/Lab004_Keywords_Identifiers.java
+++ src/exp1_Java_Basic/Lab004_Keywords_Identifiers.java
@@ -4,6 +4,7 @@ public class Lab004_Keywords_Identifiers {
 
         public static void main(String[] args) {
             System.out.println("Hi,Digambar");
+            System.out.println("Hi,Vasant");
         }
     }
 
