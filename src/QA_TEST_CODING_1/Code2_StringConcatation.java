851d7844c2e4b18e8b86d1a9bc14d9f52295ba99
Digambarh100 <digambarh100@gmail.com>
2025-06-19 08:10:11 +0530

    code2

---
+++ src/QA_TEST_CODING_1/Code2_StringConcatation.java
@@ -0,0 +1,12 @@
+package QA_TEST_CODING_1;
+
+public class Code2_StringConcatation {
+    public static void main(String[] args) {
+        int num11 = 50;
+        int num22 = 10;
+        System.out.println("Addition : " + (num11 + num22));
+        System.out.println("Concatenation: " + num11 + num22);
+        System.out.println("Sum: " + (num11 + num22));
+        System.out.println(num11 + num22 + " is the sum");
+    }
+}
